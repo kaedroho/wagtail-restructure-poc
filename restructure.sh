2d29297693c7c39773238a1d7679e3a8c6875cd0
Karl Hobley <karl@torchbox.com>
2021-10-15 13:00:27 +0100

    Simplification

--- restructure.sh
+++ restructure.sh
@@ -8,63 +8,68 @@ cd wagtail
 
 git checkout b3366749d9b068ea1bc4ae01495e9d1b77ae2333 -b restructure
 
+echo "/.ropeproject" >> .gitignore
+
+
+# Apply A couple of PRs that make the restructuring easier
+# 7277 "Move Query model to search promotions" https://github.com/wagtail/wagtail/pull/7277
+# 7564 "Move UserProfile into wagtailcore" https://github.com/wagtail/wagtail/pull/7564
+
 git apply --reject --whitespace=fix ../patches/pr7277.patch
 git add .
 git commit -m "Apply PR7277"
 
 git apply --reject --whitespace=fix ../patches/pr7564.patch
+# git apply doesn't seem to like deleting stuff
+rm wagtail/users/models.py
 git add .
 git commit -m "Apply PR7564"
 
-mv wagtail/api wagtail/core/api
-find . -name '*.py' -exec sed -i 's/wagtail.api/wagtail.core.api/g' {} \;
-git add .
-git commit -m "Move wagtail.api to wagtail.core.api"
 
-mv wagtail/admin wagtail/core/admin
-find . -name '*.py' -exec sed -i 's/wagtail.admin/wagtail.core.admin/g' {} \;
-git add .
-git commit -m "Move wagtail.admin to wagtail.core.admin"
+# Rename wagtail.core to wagtail
+# This part starts off with a few renames to resolves conflicts, then moves everthing under wagtail/core to the top level
 
-git apply --reject --whitespace=fix ../patches/merge-admin-and-core-signal-handlers.patch
-git add .
-git commit -m "Merge admin and core signal handlers"
-
-cat wagtail/core/admin/wagtail_hooks.py >> wagtail/core/wagtail_hooks.py
-git apply --reject --whitespace=fix ../patches/fixup-core-wagtail_hooks.patch
-git add .
-git commit -m "Merge admin and core Wagtail hooks"
-
-mv wagtail/core/admin/edit_handlers.py wagtail/core/edit_handlers.py
-git apply --reject --whitespace=fix ../patches/fix-core-edit_handlers-relative-imports.patch
-find . -name '*.py' -exec sed -i 's/wagtail.core.admin.edit_handlers/wagtail.core.edit_handlers/g' {} \;
-git apply --reject --whitespace=fix ../patches/move-edit_handlers-import-into-apps.patch
-git add .
-git commit -m "Move edit handlers to core"
-
-mv wagtail/core/utils.py wagtail/core/coreutils.py
-find . -name '*.py' -exec sed -i 's/wagtail.core.utils/wagtail.core.coreutils/g' {} \;
-sed -i 's/..core.utils/wagtail.core.coreutils/g' wagtail/embeds/embeds.py
+roper rename-module --module wagtail/core/utils.py --to-name coreutils --do
+find . -name '*.rst' -exec sed -i 's/wagtail.core.utils/wagtail.core.coreutils/g' {} \;
+find . -name '*.md' -exec sed -i 's/wagtail.core.utils/wagtail.core.coreutils/g' {} \;
 git add .
 git commit -m "Move core.utils to core.coreutils"
 
-mv wagtail/core/sites.py wagtail/core/siteutils.py
-find . -name '*.py' -exec sed -i 's/wagtail.core.sites/wagtail.core.siteutils/g' {} \;
+roper rename-module --module wagtail/core/sites.py --to-name siteutils --do
+find . -name '*.rst' -exec sed -i 's/wagtail.core.sites/wagtail.core.siteutils/g' {} \;
+find . -name '*.md' -exec sed -i 's/wagtail.core.sites/wagtail.core.siteutils/g' {} \;
 git add .
 git commit -m "Move core.sites to core.siteutils"
 
-mv wagtail/tests wagtail/test
+roper rename-module --module wagtail/tests --to-name test --do
+# Need to update .py files here since wagtail.tests appears a lot in strings
+# Also, escaping . for this one since wagtail_tests appears often
 find . -name '*.py' -exec sed -i 's/wagtail\.tests/wagtail\.test/g' {} \;
-find . -name '*.py' -exec sed -i "s/os.path.join(WAGTAIL_ROOT, 'tests', 'testapp', 'jinja2_templates'),/os.path.join(WAGTAIL_ROOT, 'test', 'testapp', 'jinja2_templates'),/g" {} \;
+find . -name '*.rst' -exec sed -i 's/wagtail\.tests/wagtail\.test/g' {} \;
+find . -name '*.md' -exec sed -i 's/wagtail\.tests/wagtail\.test/g' {} \;
+sed -i "s/os.path.join(WAGTAIL_ROOT, 'tests', 'testapp', 'jinja2_templates'),/os.path.join(WAGTAIL_ROOT, 'test', 'testapp', 'jinja2_templates'),/g" wagtail/test/settings.py
 git add .
 git commit -m "Move tests to test"
 
 cat wagtail/core/__init__.py >> wagtail/__init__.py
 rm wagtail/core/__init__.py
-git add .
-git commit -m "Merge wagtail.core.__init__ with wagtail.__init__"
-
-mv wagtail/core/* wagtail
+git apply --reject --whitespace=fix ../patches/concantenate-core-init.patch
+mv -n wagtail/core/* wagtail
 find . -name '*.py' -exec sed -i 's/wagtail\.core/wagtail/g' {} \;
+find . -name '*.rst' -exec sed -i 's/wagtail\.core/wagtail/g' {} \;
+find . -name '*.md' -exec sed -i 's/wagtail\.core/wagtail/g' {} \;
+sed -i "s/from ..core.coreutils/from wagtail.coreutils/g" wagtail/embeds/embeds.py
+sed -i "s/self.assertRaises(ValueError, resolve_model_string, 'wagtail.Page')/self.assertRaises(ValueError, resolve_model_string, 'wagtail.core.Page')/g" wagtail/tests/tests.py
 git add .
 git commit -m "Move wagtail.core to wagtail"
+
+
+# Merge admin into core
+
+roper move-module --source wagtail/admin/edit_handlers.py --target wagtail --do
+find . -name '*.py' -exec sed -i 's/wagtail.admin.edit_handlers/wagtail.edit_handlers/g' {} \;
+find . -name '*.rst' -exec sed -i 's/wagtail.admin.edit_handlers/wagtail.edit_handlers/g' {} \;
+find . -name '*.md' -exec sed -i 's/wagtail.admin.edit_handlers/wagtail.edit_handlers/g' {} \;
+
+git add .
+git commit -m "Move edit handlers to wagtail.edit_handlers"
