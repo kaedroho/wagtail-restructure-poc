79ee8f4db462ba557b756f001e00b3c25bd14fca
Karl Hobley <karl@kaed.uk>
2022-03-31 13:25:07 +0000

    General reorg

--- restructure.sh
+++ restructure.sh
@@ -8,43 +8,120 @@ cd wagtail
 
 git checkout 97e781e31c3bb227970b174dc16fb7febb630571 -b restructure
 
-echo "/.ropeproject" >> .gitignore
-
-
 # Reorganise
 
 poetry run roper move-module --source wagtail/compat.py --target wagtail/utils --do
 poetry run roper move-module --source wagtail/telepath.py --target wagtail/utils --do
 poetry run roper move-module --source wagtail/treebeard.py --target wagtail/utils --do
 poetry run roper move-module --source wagtail/url_routing.py --target wagtail/utils --do
-poetry run roper move-module --source wagtail/whitelist.py --target wagtail/utils --do
-poetry run isort -rc wagtail
+poetry run roper move-module --source wagtail/log_actions.py --target wagtail/utils --do
+poetry run roper move-module --source wagtail/coreutils.py --target wagtail/utils --do
+poetry run roper move-module --source wagtail/widget_adapters.py --target wagtail/utils --do
+poetry run roper move-module --source wagtail/models/copying.py --target wagtail/utils --do
+poetry run roper move-module --source wagtail/admin/staticfiles.py --target wagtail/utils --do
+poetry run isort .
+poetry run black .
 git add .
-git checkout -- docs/releases
+git checkout -- docs/releases ./ropeproject
 git commit -m "Move some modules into utils"
 
-poetry run roper move-module --source wagtail/query.py --target wagtail/models --do
-sed -i 's/from wagtail.models.sites import Site/from wagtail.models.sites import Site/g' wagtail/models/query.py
-sed -i 's/from .models import WorkflowState/from .workflows import WorkflowState/g' wagtail/models/query.py
-sed -i 's/from .models import PageRevision/from .pages import PageRevision/g' wagtail/models/query.py
-sed -i 's/from wagtail.models import Page/from .pages import Page/g' wagtail/models/query.py
-sed -i 's/from wagtail.models import PageViewRestriction/from .pages import PageViewRestriction/g' wagtail/models/query.py
-find . -name '*.py' -exec sed -i 's/wagtail\.query/wagtail\.models\.query/g' {} \;
-poetry run isort -rc wagtail
+touch wagtail/utils/models.py
+poetry run roper move-by-name --name get_streamfield_names --source wagtail/models/__init__.py --target wagtail/utils/models.py --do
+poetry run roper move-by-name --name get_concrete_descendants --source wagtail/workflows.py --target wagtail/utils/models.py --do
+poetry run roper move-by-name --name resolve_model_string --source wagtail/utils/coreutils.py --target wagtail/utils/models.py --do
+poetry run roper move-by-name --name get_model_string --source wagtail/utils/coreutils.py --target wagtail/utils/models.py --do
+poetry run isort .
+poetry run black .
 git add .
-git checkout -- docs/releases
+git checkout -- docs/releases ./ropeproject
+git commit -m "Create utils/models.py"
+
+touch wagtail/utils/i18n.py
+poetry run roper move-by-name --name reset_cache --source wagtail/utils/coreutils.py --target wagtail/utils/i18n.py --do
+poetry run roper move-by-name --name get_locales_display_names --source wagtail/utils/coreutils.py --target wagtail/utils/i18n.py --do
+poetry run roper move-by-name --name get_supported_content_language_variant --source wagtail/utils/coreutils.py --target wagtail/utils/i18n.py --do
+poetry run roper move-by-name --name get_content_languages --source wagtail/utils/coreutils.py --target wagtail/utils/i18n.py --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Create utils/i18n.py"
+
+touch wagtail/utils/text.py
+poetry run roper move-by-name --name camelcase_to_underscore --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run roper move-by-name --name string_to_ascii --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run roper move-by-name --name SLUGIFY_RE --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run roper move-by-name --name cautious_slugify --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run roper move-by-name --name safe_snake_case --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Create utils/text.py"
+
+touch wagtail/utils/html.py
+poetry run roper move-by-name --name escape_script --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run roper move-by-name --name SCRIPT_RE --source wagtail/utils/coreutils.py --target wagtail/utils/text.py --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Create utils/html.py"
+
+touch wagtail/utils/batch.py
+poetry run roper move-by-name --name BatchCreator --source wagtail/utils/coreutils.py --target wagtail/utils/batch.py --do
+poetry run roper move-by-name --name BatchProcessor --source wagtail/utils/coreutils.py --target wagtail/utils/batch.py --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Create utils/batch.py"
+
+poetry run roper rename-module --module wagtail/utils/coreutils.py --to-name misc --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Rename utils/coreutils.py to utils/misc.py"
+
+poetry run roper move-by-name --name deep_update --source wagtail/utils/utils.py --target wagtail/utils/misc.py --do
+rm wagtail/utils/utils.py
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Move deep_update into utils/misc.py"
+
+poetry run roper rename-module --module wagtail/utils/log_actions.py --to-name logging --do
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
+git commit -m "Rename utils/log_actions.py to utils/logging.py"
+
+poetry run roper move-module --source wagtail/query.py --target wagtail/models --do
+find . -name '*.py' -exec sed -i 's/wagtail\.query/wagtail\.models\.query/g' {} \;
+poetry run isort .
+poetry run black .
+git add .
+git checkout -- docs/releases ./ropeproject
 git commit -m "Move query into models"
 
-poetry run roper rename-module --module wagtail/log_actions.py --to-name logging --do
-poetry run isort -rc wagtail
+poetry run roper move-module --source wagtail/whitelist.py --target wagtail/rich_text --do
+poetry run roper rename-module --module wagtail/rich_text/whitelist.py --to-name cleaner --do
+find . -name '*.py' -exec sed -i 's/Whitelister/HTMLCleaner/g' {} \;
+find . -name '*.md' -exec sed -i 's/Whitelister/HTMLCleaner/g' {} \;
+find . -name '*.rst' -exec sed -i 's/Whitelister/HTMLCleaner/g' {} \;
+poetry run isort .
+poetry run black .
 git add .
-git checkout -- docs/releases
-git commit -m "Rename log_actions to logging"
+git checkout -- docs/releases ./ropeproject
+git commit -m "Move/Rename whitelister.Whitelist to rich_text.cleaner.HTMLCleaner"
 
 poetry run roper move-by-name --name PageClassNotFoundError --source wagtail/exceptions.py --target wagtail/admin/views/pages/edit.py --do
 rm wagtail/exceptions.py
-poetry run isort -rc wagtail
+poetry run isort .
+poetry run black .
 git add .
-git checkout -- docs/releases
+git checkout -- docs/releases ./ropeproject
 git commit -m "Move PageClassNotFoundError to page edit view (the only place where it is thrown)"
-# TODO: Fixup import https://github.com/wagtail/wagtail/pull/7656/commits/e93e9667a6e01c06f659babf95ce659c4a4611e0#diff-af279db108ccf16fa62b926938e314919e27b6df8919fa2dc07a7124f17dc9bdR16
